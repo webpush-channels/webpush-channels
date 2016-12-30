6d6fef49fa97d00ab9ca3c28899ef1bf2a48347e
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-12-30 17:12:31 +0100

    remove extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,11 +27,11 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.githubpages']
+# extensions = ['sphinx.ext.autodoc',
+#               'sphinx.ext.doctest',
+#               'sphinx.ext.todo',
+#               'sphinx.ext.coverage',
+#               'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
