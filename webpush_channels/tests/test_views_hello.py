2d8ebe35defed6c346133dbad30e660f62e384d7
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-01-13 21:34:31 +0530

    Added tests to check heartbeat and lheartbeat

--- webpush_channels/tests/test_views_hello.py
+++ webpush_channels/tests/test_views_hello.py
@@ -10,3 +10,22 @@ class HelloViewTest(BaseWebTest, unittest.TestCase):
 		self.assertEqual(response.json['project_docs'],
                          'http://webpush-channels-broadcasting.readthedocs.io/')
 		self.assertEqual(response.json['url'], 'http://localhost:9999/v0/')
+
+class LoadBalancerHeartbeat(BaseWebTest, unittest.TestCase):
+    def test_checks_if_lheartbeat_is_working(self):
+        resp = self.app.get('/__lbheartbeat__')
+        self.assertEqual(resp.json, {})
+
+class Heartbeat(BaseWebTest, unittest.TestCase):
+	def test_returns_storage_true_if_ok(self):
+		response = self.app.get('/__heartbeat__')
+		self.assertEqual(response.json['storage'], True)
+
+	def test_returns_cache_true_if_ok(self):
+		response = self.app.get('/__heartbeat__')
+		self.assertEqual(response.json['cache'], True)
+
+	def test_successful_if_one_heartbeat_is_none(self):
+		self.app.app.registry.heartbeats['probe'] = lambda r: None
+		response = self.app.get('/__heartbeat__', status=200)
+		self.assertEqual(response.json['probe'], None)
