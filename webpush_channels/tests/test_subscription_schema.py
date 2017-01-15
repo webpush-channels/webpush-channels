c01151cbc79fb21f3563c160a4556677b503137a
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-01-16 02:22:30 +0530

    Modified schema of push subscritions

--- webpush_channels/tests/test_subscription_schema.py
+++ webpush_channels/tests/test_subscription_schema.py
@@ -7,15 +7,11 @@ class SubscriptionSchemaTest(unittest.TestCase):
 		self.schema = self.schema.bind()
 		keys = dict(auth="authToken",
 				p256dh="encryptionKey")
-		push = dict(endpoint="https://push.mozilla.com",
+		data = dict(endpoint="https://push.mozilla.com",
         			keys=keys)
-		actions = ['write']
-		path = '/buckets/blocklists/collections/*/records'
-		triggers = dict(path=actions)
-		self.record = dict(push=push,
-        				   triggers=triggers)
+		self.record = dict(data=data)
 		self.deserialized = self.schema.deserialize(self.record)
 
 	def test_record_validation(self):
-		self.assertEqual(self.deserialized['push'], self.record['push'])
+		self.assertEqual(self.deserialized['data'], self.record['data'])
 
--- webpush_channels/views/subscriptions.py
+++ webpush_channels/views/subscriptions.py
@@ -6,14 +6,12 @@ class KeySchema(colander.MappingSchema):
     auth = colander.SchemaNode(colander.String())
     p256dh = colander.SchemaNode(colander.String())
 
-class PushSchema(colander.MappingSchema):
+class DataSchema(colander.MappingSchema):
     endpoint = colander.SchemaNode(colander.String(), validator=colander.url)
     keys = KeySchema()
 
 class SubscriptionSchema(ResourceSchema):
-    push = PushSchema()
-    triggers = colander.SchemaNode(colander.Mapping(unknown='preserve'),
-                                   validator=colander.All)
+    data = DataSchema()
 
 @register(record_methods=('GET', 'PATCH', 'DELETE'))
 class Subscription(UserResource):
