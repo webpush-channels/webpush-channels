a68c567c32c30fdb8003c2276cb15e2e02590ab2
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-01-16 22:19:47 +0530

    corrections in indentation

--- README.rst
+++ README.rst
@@ -4,4 +4,3 @@ WebPush Channels Broadcast
 Broadcast notification to multiple WebPush subscriptions.
 
 - `Documentation <http://webpush-channels-broadcasting.readthedocs.io/en/latest/>`_
-
--- webpush_channels/__init__.py
+++ webpush_channels/__init__.py
@@ -21,29 +21,29 @@ logger = logging.getLogger(__name__)
 
 
 DEFAULT_SETTINGS = {
-	'kinto.core.paginate_by': 100,
-	'flush_endpoint_enabled': False,
-	'retry_after_seconds': 3,
-	'cache_backend': 'kinto.core.cache.memory',
-	'permission_backend': 'kinto.core.permission.memory',
-	'storage_backend': 'kinto.core.storage.memory',
-	'project_docs': 'http://webpush-channels-broadcasting.readthedocs.io/',
-	'multiauth.authorization_policy': (
+    'kinto.core.paginate_by': 100,
+    'flush_endpoint_enabled': False,
+    'retry_after_seconds': 3,
+    'cache_backend': 'kinto.core.cache.memory',
+    'permission_backend': 'kinto.core.permission.memory',
+    'storage_backend': 'kinto.core.storage.memory',
+    'project_docs': 'http://webpush-channels-broadcasting.readthedocs.io/',
+    'multiauth.authorization_policy': (
 		'kinto.authorization.AuthorizationPolicy'),
-	'http_api_version': HTTP_API_VERSION
+    'http_api_version': HTTP_API_VERSION
 }
 
 
 def main(global_config, config=None, **settings):
-	if not config:
-		config = Configurator(settings=settings)
+    if not config:
+        config = Configurator(settings=settings)
 
-	config.add_settings({'kinto.project_name': 'webpush-channels'})
+    config.add_settings({'kinto.project_name': 'webpush-channels'})
 
-	kinto.core.initialize(config,
-                        version=__version__,
-                        default_settings=DEFAULT_SETTINGS)
+    kinto.core.initialize(config,
+			    version=__version__,
+			    default_settings=DEFAULT_SETTINGS)
 
-	config.scan("webpush_channels.views")
-	app = config.make_wsgi_app()
-	return kinto.core.install_middlewares(app, settings)
+    config.scan("webpush_channels.views")
+    app = config.make_wsgi_app()
+    return kinto.core.install_middlewares(app, settings)
