8eba9b25523c613d77c54c56bfd3e728a0b86d90
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-01-24 23:07:20 +0530

    Added some tests to cover error handling

--- webpush_channels/tests/test_views_channel.py
+++ webpush_channels/tests/test_views_channel.py
@@ -1,8 +1,11 @@
 import unittest
+import mock
 from .support import BaseWebTest
+from kinto.core.storage import exceptions as storage_exceptions
 
 
 class ChannelRegistrationTest(BaseWebTest, unittest.TestCase):
+
     channel_url = '/channels/food'
     channel_registration_url = '/channels/food/registration'
 
@@ -19,6 +22,28 @@ class ChannelRegistrationTest(BaseWebTest, unittest.TestCase):
         self.app.delete(self.channel_registration_url, headers=self.headers, status=202)
         resp = self.app.get(self.channel_url, headers=self.headers, status=200)
         assert resp.json['data']['registrations'] == 0
+
+    def test_remove_non_existent_registration(self):
+        self.assertRaises(self.app.delete(self.channel_registration_url,
+                                          headers=self.headers, status=500))
+
+
+class BackendErrorTest(BaseWebTest, unittest.TestCase):
+
+    channel_registration_url = '/channels/food/registration'
+
+    def setUp(self):
+        super(BackendErrorTest, self).setUp()
+        self.error = storage_exceptions.BackendError(ValueError())
+        self.storage_error_patcher = mock.patch(
+            'kinto.core.storage.memory.Storage.create',
+            side_effect=self.error)
+
+    def test_backend_errors_are_served_as_503(self):
+        with self.storage_error_patcher:
+            self.app.put(self.channel_registration_url,
+                         headers=self.headers,
+                         status=503)
 
 
 class ChannelsTest(BaseWebTest, unittest.TestCase):
