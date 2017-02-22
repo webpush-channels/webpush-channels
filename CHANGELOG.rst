c0b339ad93ddbd9bde9e1381047185c667283aa4
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-02-22 20:51:51 +0530

    Back to development: 0.2.0

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -3,6 +3,12 @@ Changelog
 
 This document describes changes between each past release.
 
+0.2.0 (unreleased)
+------------------
+
+- Nothing changed yet.
+
+
 0.1.0 (2017-02-22)
 ------------------
 
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ ENTRY_POINTS = {
     ]}
 
 setup(name='webpush-channels',
-      version='0.1.0',
+      version='0.2.0.dev0',
       description='Broadcast notifications to multiple WebPush subscribers.',
       long_description=README + "\n\n" + CHANGELOG,
       license='Apache License (2.0)',
